8a5c0c9ab331d6ef9b6662973f7e89e7d8b40e47
charlie <1151832099@qq.com>
2016-08-01 11:44:46 +0800

    a simple example

---
+++ src/test/java/com/github/charlie/jedis/JedisTest.java
@@ -0,0 +1,30 @@
+package com.github.charlie.jedis;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+
+public class JedisTest {
+
+    @Test
+    public void test() {
+        Set<HostAndPort> hosts = new HashSet<HostAndPort>();
+        hosts.add(new HostAndPort("192.168.158.128", 7001));
+        hosts.add(new HostAndPort("192.168.158.128", 7002));
+        hosts.add(new HostAndPort("192.168.158.128", 7003));
+        JedisCluster jc = new JedisCluster(hosts);
+        String value = jc.get("k");
+        System.out.println(value);
+        try {
+            jc.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
